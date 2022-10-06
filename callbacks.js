fa8f1ded77a5fb6c96c7099b5131cf840a7b3ad8
Moisés R. Martins <90099815+moisesrmartins@users.noreply.github.com>
2022-10-06 15:11:42 -0300

    React_8_Callbacks Parte 2: Callbacks e in.txt

--- callbacks.js
+++ callbacks.js
@@ -3,3 +3,21 @@ const fs = require("fs");
 console.log(1);
 console.log(2);
 console.log(3);
+
+function callback(error, content) {
+  console.log(error, content);
+}
+
+fs.readFile("./in1.txt", callback);
+
+function callbacks(mistake, contents) {
+  console.log(mistake, String(contents));
+}
+
+fs.readFile("./in2.txt", callbacks);
+
+function callbacks(mistake, contents) {
+  console.log(mistake, String(contents));
+}
+
+fs.readFile("./in3.txt", callbacks);
